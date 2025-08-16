716be51eb0334d2177bfd38dba470bf5494c20e4
nupurchoksi <nupurnchoksi@gmail.com>
2025-08-16 04:42:02 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,27 @@
+const quizQuestions = [
+    {
+        question: "What is Ted’s profession?",
+        options: ["<Architect>", "<Lawyer>", "<Journalist>", "<Doctor>"],
+        answer: 0
+    },
+    {
+        question: "What is Barney Stinson’s famous catchphrase?",
+        options: ["It’s going to be awesome!", "Suit up!", "Legendary!", "Challenge accepted!"],
+        answer: 1
+    },
+    {
+        question: "Which instrument does Ted steal for Robin in Season 1?",
+        options: ["Red trumpet", " Blue French horn", "Yellow saxophone", "Green guitar"],
+        answer: 1
+    },
+    {
+        question: "What is the name of Marshall and Lily’s first child?",
+        options: [" Daisy", "Penny", "Marvin", " Ellie"],
+        answer: 2
+    },
+    {
+        question: "Where do the gang spend most of their time hanging out?",
+        options: ["McLaren’s Pub", "Central Perk", "The Drunken Clam", "Cheers Bar"],
+        answer: 0
+    }
+];
